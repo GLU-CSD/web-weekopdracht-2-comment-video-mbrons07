51d964aa3e1c43ac69f720fd627f609895953ef8
mbrons07 <mick.brons@outlook.com>
2024-12-13 13:32:43 +0100

    final result

--- index.php
+++ index.php
@@ -2,17 +2,13 @@
 include("config.php");
 include("reactions.php");
 
-$getReactions = Reactions::getReactions();
-//uncomment de volgende regel om te kijken hoe de array van je reactions eruit ziet
-// echo "<pre>".var_dump($getReactions)."</pre>";
 
 if(!empty($_POST)){
-
     //dit is een voorbeeld array.  Deze waardes moeten erin staan.
     $postArray = [
-        'name' => "Ieniminie",
+        'name' => $_POST['naam'],
         'email' => "ieniminie@sesamstraat.nl",
-        'message' => "Geweldig dit"
+        'message' => $_POST['message']
     ];
 
     $setReaction = Reactions::setReaction($postArray);
@@ -20,9 +16,11 @@ if(!empty($_POST)){
     if(isset($setReaction['error']) && $setReaction['error'] != ''){
         prettyDump($setReaction['error']);
     }
-    
-
 }
+
+$getReactions = Reactions::getReactions();
+//uncomment de volgende regel om te kijken hoe de array van je reactions eruit ziet
+
 
 ?>
 <!DOCTYPE html>
@@ -31,19 +29,57 @@ if(!empty($_POST)){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Youtube remake</title>
+    <style>
+#review{
+    width:100px;
+    height:100px;
+}
+body{
+    background: red;
+    font-family: Arial, Helvetica, sans-serif;
+}
+.bericht{
+    background: black;
+    box-shadow: 2px 2px 2px black;
+    border-radius: 10px;
+    padding: 10px;
+    margin:10px;
+    display: inline-block;
+    color: red;
+}
+    </style>
 </head>
 <body>
-<iframe width="560" height="315" src="https://www.youtube.com/embed/kf1gHqritRQ?si=7hVyb22P5y5lnrUf" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/kf1gHqritRQ?si=9QDcJMn99ZIuV-FB" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 
-    <h2>hier komt het formulier om een bericht te bersturen</h2>
-    <form action="" method="post">
-        <input type="text" placeholder="..vul hier je naam in" name="name">
+   
+   <h2>hier komt het formulier om een bericht te versturen</h2>
+   <form action="" method="post">
+        <input required type="text" placeholder="..vul hier je naam in" name="naam">
         <br>
-        <textarea id="w3review" placeholder="..vul hier je bericht in" name="message" name="w3review" rows="4" cols="50"></textarea>
-    <h2>Hieronder komen reacties</h2>
-    <p> this is peak music this is peak fiction </p>
-    <p> absolute cinema </p>
-    <p> best final boss music ever (edited) </p>
+        <textarea required id="review" placeholder="..vul hier je bericht in" name="message"></textarea>
+        <br>
+        <input type="submit" value="versturen">
+
+    </form>
+   
+        <h2>Hieronder komen reacties</h2>
+    <p>Maak hier je eigen pagina van aan de hand van de opdracht</p>
+
+<?php
+//echo "<pre>".var_dump($getReactions)."</pre>";
+
+echo ("<h2>Er zijn ".count($getReactions)." reactie</h2>");
+
+for ($i=0; $i < count($getReactions); $i++) { 
+    echo("<div class='bericht'>");
+    echo($getReactions[$i]['name']." -- ");
+    echo($getReactions[$i]['message']."<br>");
+    echo("</div>");    
+}
+?>
+
+
 </body>
 </html>
 
